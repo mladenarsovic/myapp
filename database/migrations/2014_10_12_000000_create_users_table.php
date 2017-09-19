1ac1b63141825c8af9c2e531a81c36681468631c
Mladen <djoml8bre@gmail.com>
2017-09-19 09:24:19 +0200

    Migration added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('id');            
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company');
+            $table->string('country');            
             $table->rememberToken();
             $table->timestamps();
         });
