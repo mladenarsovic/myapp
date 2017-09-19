32918f6f6559d01a34ab5b89ff2f937bc8ccae2c
Mladen <djoml8bre@gmail.com>
2017-09-19 09:46:33 +0200

    Added command for deleting UsersTableSeeder

---
+++ app/Console/Commands/DeleteUsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\User;
+
+class DeleteUsersTableSeeder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete:seeder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete Users Table Seeded Data';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $data = User::all();
+
+        foreach($data as $row){
+            $row->delete();
+        }
+    }
+}
